df330eaef7219a66a2a33d5c50cd9462ee7a24e3
Mark@BT_1N3_12 <sample@bths.edu>
2025-03-28 10:48:26 -0400

    Completed Letter superclass

--- Letter.java
+++ Letter.java
@@ -1,12 +1,16 @@
 public class Letter {
+    /** Letter **/
     private String c;
-    public Letter(String c) {
+    /** Color ANSI string from Colors */
+    private String color;
+    public Letter(String c, String color) {
+        this.color = color;
         this.c = c;
     }
 
     @Override
     public String toString() {
-        return c;
+        return color + c + Colors.RESET;
     }
 
 
