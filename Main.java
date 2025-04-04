d887a173e51b0a1a833ef2bc6259334f125cc488
BT_1N3_11 <sample@bths.edu>
2025-04-04 10:53:51 -0400

    Had to move the play again code out of the main - Imran Zahid

--- Main.java
+++ Main.java
@@ -4,12 +4,5 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         Game g = new Game();
         g.start();
-        //System.out.println("Want to play again?");
-        //String answer = scan.nextLine();
-        //while (!answer.equals("n")){
-        //    g = new Game();
-        //    g.start();
-        //}
-        //System.out.println("Bye bye");
     }
 }
