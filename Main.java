bd8b680acf571da784ebd044d3c926ba751dfdfa
Nuhas R <nuhasyt07@gmail.com>
2025-04-06 17:12:21 -0400

    removed unnecessary code in the main method

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
         Game g = new Game();
         g.start();
 
